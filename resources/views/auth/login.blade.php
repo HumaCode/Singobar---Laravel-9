f15f974a29c0f300de4474b5837a1ac8a2af6b18
humaidi <humaidizakaria1998@gmail.com>
2023-05-29 16:39:23 +0700

    Menampilkan notifikasi berhasil login dan logout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,6 +17,8 @@
     <!-- vendor css -->
     <link rel="stylesheet" href="{{ asset('backend') }}/assets//css/style.css">
 
+    {{-- toaster --}}
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 
 
 
@@ -71,6 +73,30 @@
 <script src="{{ asset('backend') }}/assets//js/vendor-all.min.js"></script>
 <script src="{{ asset('backend') }}/assets//js/plugins/bootstrap.min.js"></script>
 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+    @if(Session::has('message'))
+        var type = "{{ Session::get('alert-type','info') }}"
+        switch(type){
+            case 'info':
+            toastr.info(" {{ Session::get('message') }} ");
+            break;
+        
+            case 'success':
+            toastr.success(" {{ Session::get('message') }} ");
+            break;
+        
+            case 'warning':
+            toastr.warning(" {{ Session::get('message') }} ");
+            break;
+        
+            case 'error':
+            toastr.error(" {{ Session::get('message') }} ");
+            break; 
+        }
+        @endif 
+</script>
 
 
 
